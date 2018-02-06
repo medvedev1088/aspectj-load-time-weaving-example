75efbbdf01906c2652612d796255fe3521083f4c
Evgeny Medvedev <evgeny@kitchenup.com>
2018-02-06 19:13:56 +0700

    Update lib versions

--- src/main/java/com/example/aspectj/DateTimeToStringAspect.java
+++ src/main/java/com/example/aspectj/DateTimeToStringAspect.java
@@ -8,16 +8,16 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class DateTimeToStringAspect {
 
-	public static final String TO_STRING_RESULT = "test";
+    public static final String TO_STRING_RESULT = "test";
 
-	@Pointcut("execution(* org.joda.time.base.AbstractDateTime.toString())")
-	public void dateTimeToString() {
-	}
+    @Pointcut("execution(* org.joda.time.base.AbstractDateTime.toString())")
+    public void dateTimeToString() {
+    }
 
-	@Around("dateTimeToString()")
-	public Object toLowerCase(ProceedingJoinPoint joinPoint) throws Throwable {
-		Object ignoredToStringResult = joinPoint.proceed();
-		System.out.println("DateTime#toString() has been invoked: " + ignoredToStringResult);
-		return TO_STRING_RESULT;
-	}
+    @Around("dateTimeToString()")
+    public Object toLowerCase(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object ignoredToStringResult = joinPoint.proceed();
+        System.out.println("DateTime#toString() has been invoked: " + ignoredToStringResult);
+        return TO_STRING_RESULT;
+    }
 }
--- src/test/java/com/example/aspectj/DateTimeToStringAspectTest.java
+++ src/test/java/com/example/aspectj/DateTimeToStringAspectTest.java
@@ -2,12 +2,13 @@ package com.example.aspectj;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DateTimeToStringAspectTest {
-	@Test
-	public void testDateTimeToString() throws Exception {
-		assertThat(new DateTime().toString(), is(DateTimeToStringAspect.TO_STRING_RESULT));
-	}
+    @Test
+    public void testDateTimeToString() throws Exception {
+        assertThat(new DateTime().toString(), is(DateTimeToStringAspect.TO_STRING_RESULT));
+    }
 }
